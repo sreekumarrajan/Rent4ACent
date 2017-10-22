4ef47623e35cdc282cfa1c2766fe67fd24e2d4b0
Sreekumar Rajan <sreekumar.rajan@gmail.com>
2017-10-21 22:07:48 -0400

    Initial generation of angular 4 cli project

---
+++ Rent4ACent/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('rent4-acent App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
